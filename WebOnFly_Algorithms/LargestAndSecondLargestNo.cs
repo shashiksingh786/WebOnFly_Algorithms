734e3fc0e1862edc0bb15dc1fec93249e089b1e5
Shashi Kiran Singh <to.shashi7886@hotmail.com>
2024-09-29 13:34:19 +0530

    Add project files.

---
+++ WebOnFly_Algorithms/LargestAndSecondLargestNo.cs
@@ -0,0 +1,46 @@
+﻿namespace WebOnFly_Algorithms
+{
+    static internal class LargestAndSecondLargestNo
+    {
+
+        /// <summary>
+        /// 1. Write an algorithm which finds the position of largest and second largest element in an array A with n>1 elements. Give its Complexity
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static void FindLargestAndSecondLargestNoInArray(int[] arr)
+        {
+             if(arr.Length == 0) { Console.WriteLine("Empty Array List");  return; }
+
+            int first = arr[0];
+            int second = arr[1];
+
+            int l1 = first;
+            int l2 = second;
+            if (first < second)
+            {
+                l1 = second;
+                l2 = first;
+            }
+
+            for (int k = 2; k < arr.Length; k++)
+            {
+                if (first < arr[k])
+                {
+                    second = first;
+                    first = arr[k];
+
+                    l2 = second;
+                    l1 = first;
+                }
+                else if (second < arr[k])
+                {
+                    second = arr[k];
+                    l2 = second;
+                }
+            }
+            Console.WriteLine($"Largest No. is {l1} and Second Largest No. is {l2}");
+
+        }
+    }
+}
---
+++ WebOnFly_Algorithms/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+using WebOnFly_Algorithms;
+
+Console.WriteLine("Hello, World!");
+int[] input = [1, 4, 70, 3, 6, 8, 44, 67, 33, 88, 48];
+LargestAndSecondLargestNo.FindLargestAndSecondLargestNoInArray(input);
+
